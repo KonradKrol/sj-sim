bd35ed06752247e5fa8b6f4c0a24191e26645fa3
Konrad <konradkrol110@gmail.com>
2026-08-06 13:59:15 +0200

    Keep the help title visible

--- source/UI/HelpWindow.cpp
+++ source/UI/HelpWindow.cpp
@@ -8,7 +8,7 @@ HelpWindow::HelpWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     ui->label_title->setWordWrap(true);
-    ui->label_title->setSizePolicy(QSizePolicy::Ignored, QSizePolicy::Preferred);
+    ui->label_title->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
     for(int index = 0; index < ui->tabWidget->count(); ++index)
         ui->tabWidget->setTabToolTip(index, ui->tabWidget->tabText(index));
     ui->textBrowser->setOpenLinks(false);
